e3d06654a9183d9afe8757f3c2b5e5c4c60969ba
JuanfelipeX <pipesjimenez6@gmail.com>
2023-12-02 17:28:01 -0500

    haciendo las rutas

--- ecoads-marketplace/src/app/app.routes.ts
+++ ecoads-marketplace/src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'landing-page', component: LandingPageComponent },
+    { path: '', redirectTo: 'inicio', pathMatch: 'full' },
+    // { path: '**', component: ErrorNoEncontradoComponent }, //Sitio Web No Encontrado 404
+];
